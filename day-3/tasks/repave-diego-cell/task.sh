8379bc8817589c8f7cc5a571aa42f8bc42845d96
Etourneau Gwenn <gwenn.etourneau@gmail.com>
2018-07-19 13:53:47 +0900

    Adding repaving

--- day-3/tasks/repave-diego-cell/task.sh
+++ day-3/tasks/repave-diego-cell/task.sh
@@ -1,10 +1,15 @@
-
 #!/bin/bash -eu
 
 . "$(dirname $0)"/../../../scripts/export-director-metadata
 
-bosh vms
-env
+DIEGO_CELL_BEFORE=$(bosh vms -d ${ERT_DEPLOYMENT_NAME} | grep diego_cell | grep running | wc -l)
+bosh -n recreate diego_cell -d ${ERT_DEPLOYMENT_NAME} --max-in-flight=${MAX_IN_FLIGHT}
+DIEGO_CELL_AFTER=$(bosh vms -d ${ERT_DEPLOYMENT_NAME} | grep diego_cell | grep running | wc -l)
 
+if [[ "${DIEGO_CELL_BEFORE}" !=  "${DIEGO_CELL_AFTER}" ]]; then
+  echo "Numbers of Running Diego Cell are different"
+  echo "Should be ${DIEGO_CELL_BEFORE} but found ${DIEGO_CELL_AFTER}"
+  exit 1
+fi
 
-#bosh recreate diego_cell -d ${ERT_DEPLOYMENT_NAME} --max-in-flight=${MAX_IN_FLIGHT}
+echo "Diego cell repave ${DIEGO_CELL_AFTER}"
